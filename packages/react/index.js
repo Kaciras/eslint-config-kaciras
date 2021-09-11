74d67a3e1c54ace9f8b72d6d4045018eaeb26c11
Kaciras <Kaciras@protonmail.com>
2021-09-11 14:33:58 +0800

    add `react/jsx-runtime` to react config

--- packages/react/index.js
+++ packages/react/index.js
@@ -1,24 +1,18 @@
+/*
+ * 该配置仅适用于 React 17+ 并使用 React Hooks 的项目。
+ */
 module.exports = {
 	extends: [
 		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
 		"plugin:react-hooks/recommended",
 	],
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-	},
 	settings: {
 		react: {
 			version: "detect",
 		},
 	},
 	rules: {
-		// React 17 开始使用新的 JSX 转换，无需再导入 React
-		// https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#eslint
-		"react/jsx-uses-react": "off",
-		"react/react-in-jsx-scope": "off",
-
 		// 属性等号加空格不好看，调整风格时可能会多出意外的空格，所以加上此规则。
 		"react/jsx-equals-spacing": ["error", "never"],
 	},
