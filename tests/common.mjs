25ae5a515d103c7607d2628b4f190074c1146cce
Kaciras <Kaciras@protonmail.com>
2024-04-08 18:33:17 +0800

    fix tests

--- tests/common.mjs
+++ tests/common.mjs
@@ -1,5 +1,5 @@
 import assert from "assert";
-import { ESLint, Linter } from "eslint";
+import { ESLint } from "eslint";
 
 /**
  * 计算应用于指定文件的规则，展开 extends 和 overrides 属性。
@@ -7,10 +7,10 @@ import { ESLint, Linter } from "eslint";
  * @param filename 文件名
  * @param config 规则
  */
-export function getConfig(filename, config) {
+export async function getConfig(filename, config) {
 	const eslint = new ESLint({
-		useEslintrc: false,
 		overrideConfig: config,
+		overrideConfigFile: true,
 	});
 	return eslint.calculateConfigForFile(filename);
 }
@@ -41,23 +41,27 @@ function normalize(rule) {
 
 const packages = [
 	{
-		name: "core/index.js",
-		plugins: ["@kaciras"],
+		name: "../configs/core/index.js",
+		plugins: ["kaciras", "stylistic"],
 	},
 	{
-		name: "react/index.js",
-		plugins: ["react", "react-hooks"],
+		name: "../configs/react/index.js",
+		plugins: ["react"],
 	},
 	{
-		name: "jest/index.js",
+		name: "../configs/jest/index.js",
 		plugins: ["jest"],
 	},
 	{
-		name: "vue/index.js",
+		name: "../configs/vue/index.js",
 		plugins: ["vue"],
 	},
 	{
-		name: "typescript/base.js",
+		name: "../configs/vue/typescript.js",
+		plugins: ["vue"],
+	},
+	{
+		name: "../configs/typescript/index.js",
 		plugins: ["@typescript-eslint"],
 	},
 ];
@@ -69,26 +73,31 @@ packages.forEach(({ name, plugins }) => {
 	 * 验证扩展已经添加了相关的插件，无需写 plugins: [...]。
 	 */
 	it("should add plugins from " + name, async () => {
-		const config = await getConfig("foobar.tsx", {
-			extends: "./configs/" + name,
-		});
+		const { default: items } = await import(name);
 
-		assert.strictEqual(config.plugins.length, plugins.length);
-		config.plugins.forEach(p => assert(plugins.includes(p)));
+		const revolved = items
+			.map(i => i.plugins)
+			.filter(Boolean)
+			.flatMap(Object.keys);
+
+		assert.deepEqual([...new Set(revolved)], plugins);
 	});
 
 	/**
 	 * Check all rules in the config must change the rules of the base config.
 	 */
 	it("should not have redundant rules - " + name, async () => {
-		const { default: module } = await import(`../configs/${name}`);
+		const { default: items } = await import(name);
+		const base = items.slice(0, -1);
+		const custom = items.at(-1);
 
-		const { rules } = await getConfig("foobar.tsx", {
-			extends: module.extends,
-		});
+		const resolved = {};
+		for (const item of base) {
+			Object.assign(resolved, item.rules);
+		}
 
-		for (const [k, v] of Object.entries(module.rules ?? {})) {
-			const fromExt = rules[k];
+		for (const [k, v] of Object.entries(custom.rules ?? {})) {
+			const fromExt = resolved[k];
 			const [level, ...options] = normalize(v);
 
 			if (level === "off" && !fromExt) {
@@ -101,45 +110,5 @@ packages.forEach(({ name, plugins }) => {
 			}
 			assert.notDeepStrictEqual(baseOptions, options, `${k} is redundant`);
 		}
-	});
-});
-
-/**
- * 测试 typescript 包是否会和 JS 冲突，@typescript-eslint/* 规则不应用于 JS 文件。
- */
-it("should avoid conflict with typescript", async () => {
-	const code = "const unused = 0";
-	const config = await getConfig("foobar.js", {
-		env: {
-			es2021: true,
-		},
-		extends: ["./configs/typescript"],
-	});
-
-	const linter = new Linter();
-	const msg = linter.verify(code, config, "foobar.js");
-	assert.strictEqual(msg.length, 0);
-});
-
-describe("vue + typescript", () => {
-	const config = {
-		extends: [
-			"./configs/core",
-			"./configs/typescript",
-			"./configs/vue/typescript",
-		],
-		env: { es2021: true },
-	};
-
-	it("should work with SFC", async () => {
-		const resolved = await getConfig("sfc.vue", config);
-		assert(resolved.parser.includes("vue-eslint-parser"));
-		assert.strictEqual(resolved.parserOptions.parser, "@typescript-eslint/parser");
-	});
-
-	it("should work with ts file", async () => {
-		const resolved = await getConfig("code.ts", config);
-		assert(resolved.parser.includes("vue-eslint-parser"));
-		assert.strictEqual(resolved.parserOptions.parser, "@typescript-eslint/parser");
 	});
 });
--- tests/core.mjs
+++ tests/core.mjs
@@ -1,19 +1,18 @@
 import assert from "assert";
 import { Linter } from "eslint";
+import coreConfig from "../configs/core/index.js";
 import { getConfig } from "./common.mjs";
 
-const config = await getConfig("dummy.js",{
-	extends: ["./configs/core/index.js"],
-});
+const config = await getConfig("dummy.js", coreConfig);
 
 const linter = new Linter();
 
-it("should ignore unused var with name _",  async () => {
+it("should ignore unused var with name _", async () => {
 	const message = linter.verify("const _ = 1;", config);
 	assert.strictEqual(message.length, 0);
 });
 
-it("should verify unused vars",  async () => {
+it("should verify unused vars", async () => {
 	const message = linter.verify("const _foo = 1;", config);
 	assert.strictEqual(message.length, 1);
 });
